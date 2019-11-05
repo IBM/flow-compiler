6570b1309298d2b351633ce2fd98cfb29fa3613d
Cezar Pendus <cpendus@us.ibm.com>
2019-11-05 15:02:39 -0500

    Added timeout support for entries

--- flowc/flow-gserver.C
+++ flowc/flow-gserver.C
@@ -1201,7 +1201,9 @@ int flow_compiler::gc_server(std::ostream &out) {
     decltype(global_vars) local_vars;
     ServiceDescriptor const *sdp = nullptr;
     for(auto const &ne: names) if(ne.second.first == "entry") {
-        MethodDescriptor const *mdp = method_descriptor(ne.second.second);
+        auto entry_node = ne.second.second;
+        MethodDescriptor const *mdp = method_descriptor(entry_node);
+
         if(sdp != nullptr && sdp != mdp->service()) {
             error_count += 1;
             pcerr.AddError(main_file, -1, 0, "all entries must be methods of the same service");
@@ -1209,9 +1211,12 @@ int flow_compiler::gc_server(std::ostream &out) {
         sdp = mdp->service();
         append(local_vars, "ENTRY_DOT_NAME", ne.first);
         append(local_vars, "ENTRY_NAME", mdp->name());
-        append(local_vars, "ENTRY_SERVICE_NAME", mdp->full_name());
+        append(local_vars, "ENTRY_SERVICE_NAME", get_full_name(mdp->service()));
+        append(local_vars, "ENTRY_OUTPUT_TYPE", get_full_name(mdp->output_type()));
+        append(local_vars, "ENTRY_INPUT_TYPE", get_full_name(mdp->input_type()));
         append(local_vars, "ENTRY_OUTPUT_JSON_SCHEMA_C", c_escape(json_schema(mdp->output_type(), decamelize(mdp->output_type()->name()), main_description)));
         append(local_vars, "ENTRY_INPUT_JSON_SCHEMA_C", c_escape(json_schema(mdp->input_type(), to_upper(to_option(main_name)), main_description)));
+        append(local_vars, "ENTRY_TIMEOUT", std::to_string(get_blck_timeout(entry_node, default_entry_timeout)));
         std::stringstream sbuf;
         error_count += gc_server_method(sbuf, ne.first, ne.second.second);
         append(local_vars, "ENTRY_CODE", sbuf.str());
