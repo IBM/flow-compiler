b912c42456908dfdb404ec947698247d3b92ed80
Cezar Pendus <cpendus@us.ibm.com>
2021-03-09 08:41:22 -0500

    Added --help option

--- flowc/template.driver.sh
+++ flowc/template.driver.sh
@@ -32,6 +32,92 @@ export docker_compose_TIMESTAMPS=
 export docker_compose_RW_GID=$(id -g)
 export grpc_PORT=${{{NAME/id/upper}}_GRPC_PORT-{{MAIN_PORT}}}
 export rest_PORT=${{{NAME/id/upper}}_REST_PORT-{{REST_PORT}}}
+dd_display_help() {
+echo "Docker Compose/Swarm and Kubernetes configuration generator for {{NAME}}"
+echo "From {{MAIN_FILE}} ({{MAIN_FILE_TS}})"
+echo ""
+echo "Usage $(basename "$0") <up|down> [-p] [-r] [-s] [-S] [--project-name NAME] [--grpc-port PORT] [--rest-port PORT] [--htdocs DIRECTORY] {O:VOLUME_OPTION{--mount-{{VOLUME_OPTION}} DIRECTORY  }O}"
+echo "   or $(basename "$0") -k [--project-name NAME] <delete|deploy|show|config>"
+echo "   or $(basename "$0") [-T] <run|logs>"
+{V:HAVE_VOLUMES{
+echo "   or $(basename "$0") <provision> {O:VOLUME_NAME{[--mount-{{VOLUME_NAME/option}} DIRECTORY] }O}"
+}V}
+echo ""
+echo "Commands:"
+echo "   config     Display the configuration file that is sent to the underlying command"
+echo "   deploy     Deploy and start application (Kubernetes mode)"
+echo "   down       Stop the running application (Docker Compose or Swarm mode)"
+echo "   help       Display this help screen"
+echo "   logs       Display logs from all the containers (Docker Compose mode)"
+echo "   up         Start the application in the background (Docker Compose or Swarm mode)"
+{V:HAVE_VOLUMES{echo "   provision  Download thte external data into local directories so it can be mounted inside the running containers (Docker Compose only)"
+}V}
+echo "   run        Run the application in the foreground in (Docker Composer mode)"
+echo "   show       Display the current status of the application (Kubernetes mode)"
+echo ""
+echo "Options:"
+echo "    -h, --help"
+echo "        Display this help screen and exit"
+echo ""
+echo "    -k, --kubernetes"
+echo "        Enable Kubernetes mode"
+echo ""
+echo "    -p, --export-ports"
+echo "        Export the gRPC ports for all nodes"
+echo ""
+echo "    -r, --default-runtime"
+echo "        Ignore runtime settings and run with the default Docker runtime"
+echo ""
+echo "    -S, --swarm"
+echo "        Enable Docker Swarm mode"
+echo ""
+echo "    -s, --skip-provision"
+echo "        Skip checking for new data files at startup"
+echo ""
+echo "    -T, --timestamps"
+echo "        Show timestamps in logs"
+echo ""
+echo "    --project-name NAME"
+echo "        Set the Docker Compose project name to NAME (default is $kd_PROJECT_NAME)"
+echo ""
+echo "    --grpc-port PORT (or set {{NAME/id/upper}}_GRPC_PORT)"
+echo "        Set the port the GRPC server is mapped locally. The default is $grpc_PORT"
+echo ""
+echo "    --rest-port PORT (or set {{NAME/id/upper}}_REST_PORT)"
+echo "        Set the port the REST API is mapped locally. The default is $rest_PORT"
+echo ""
+echo "    --htdocs DIRECTORY (or set {{NAME/id/upper}}_HTDOCS)"
+echo "        This is a Docker Composer only option. Mount DIRECTORY inside the container to use as a custom application."
+[ ! -z "${{NAME/id/upper}}_HTDOCS" ] && echo "       Currently set to \"${{NAME/id/upper}}_HTDOCS\""
+echo ""
+{O:VOLUME_NAME{
+echo "    --mount-{{VOLUME_NAME/option-}} <DIRECTORY>  (or set {{VOLUME_NAME/id/upper}})"
+echo "        Override the path to be mounted for {{VOLUME_NAME}} (default is ${{VOLUME_NAME/id/upper}})"
+printf "        "{{VOLUME_COMMENT/sh}}"\n"
+echo ""
+echo "    --secret-{{VOLUME_NAME/option-}} <SECRET-NAME>  (or set {{VOLUME_NAME/id/upper}}_SECRET_NAME)"
+echo "        Secret name for COS access for {{VOLUME_NAME}}, default is \"${{VOLUME_NAME/id/upper}}_SECRET_NAME\""
+echo ""
+echo "    --remote-{{VOLUME_NAME/option-}} <URL> (or set {{VOLUME_NAME/id/upper}}_URL)"
+echo "        Set the URL to the remote resource for {{VOLUME_NAME}}, default is \"${{VOLUME_NAME/id/upper}}_URL\""
+echo ""
+}O}
+echo "    --htdocs-secret-name <SECRET-NAME>  (or set {{NAME/id/upper}}_HTDOCS_SECRET_NAME)"
+echo "        Secret name for COS access for the custom application files, if a COS URL was given."
+[ ! -z "${{NAME/id/upper}}_HTDOCS_SECRET_NAME" ] && echo "        Currently set to \"${{NAME/id/upper}}_HTDOCS_SECRET_NAME\""
+echo ""
+echo "    --{{NAME}}-replicas <NUMBER>  (or set {{NAME/id/upper}}_REPLICAS)"
+echo "        Number of replicas for the main pod [{{MAIN_GROUP_NODES}}]. The default is $replicas_{{NAME/id/upper}}."
+echo ""
+{G:GROUP{
+echo "    --{{GROUP}}-replicas <NUMBER>  (or set {{NAME/id/upper}}_{{GROUP/id/upper}}_REPLICAS)"
+echo "        Number or replicas for pod \"{{GROUP}}\" [{{GROUP_NODES}}]. The default is $replicas_{{NAME/id/upper}}_{{GROUP/id/upper}}."
+echo ""
+}G}
+echo "Note: To access the Artifactory or Cloud Object Store $(basename $0) looks for the environment variable API_KEY."
+echo "If not found, it looks for a file named .api-key in the current directory and then in the home directory."
+echo ""
+}
 rget_EMBEDDED_KEY_TOOL=1
 download_file() {
     {{RR_GET_SH-rget_EMBEDDED_KEY_TOOL=; source "$(dirname "$0")/rr-get.sh"}}
@@ -41,6 +127,10 @@ args=()
 while [ $# -gt 0 ]
 do
 case "$1" in
+    -h|--help)
+    dd_display_help
+    exit 0
+    ;;
 {A:VOLUME_NAME{
     --mount-{{VOLUME_NAME/option}})
     export {{VOLUME_NAME/id/upper}}="$2"
@@ -193,94 +283,14 @@ then
         export enable_custom_app=
     fi
 fi
-if [ $# -eq 0 -o "$1" == "up" -a $have_ALL_VOLUME_DIRECTORIES -eq 0 \
+if [ $# -eq 0 \
+    -o "$1" == "up" -a $have_ALL_VOLUME_DIRECTORIES -eq 0 \
     -o "$1" == "provision" -a $have_ALL_VOLUME_DIRECTORIES -eq 0  \
     -o "$1" == "run" -a $have_ALL_VOLUME_DIRECTORIES -eq 0 \
-    -o "$1" != "up" -a "$1" != "down" -a "$1" != "config" -a "$1" != "logs" -a "$1" != "provision" -a "$1" != "run" -a "$1" != "deploy" -a "$1" != "show" ]
+    -o "$1" != "help" -a "$1" != "up" -a "$1" != "down" -a "$1" != "config" -a "$1" != "logs" -a "$1" != "provision" -a "$1" != "run" -a "$1" != "deploy" -a "$1" != "show" ]
 then
-echo "Docker Compose/Swarm and Kubernetes configuration generator for {{NAME}}"
-echo "From {{MAIN_FILE}} ({{MAIN_FILE_TS}})"
-echo ""
-echo "Usage $(basename "$0") <up|run|config> [-k] [-p] [-r] [-s] [-S] [--project-name NAME] [--grpc-port PORT] [--rest-port PORT] [--htdocs DIRECTORY] {O:VOLUME_OPTION{--mount-{{VOLUME_OPTION}} DIRECTORY  }O}"
-echo "   or $(basename "$0") [-S] [--project-name NAME] <down>"
-echo "   or $(basename "$0") -k [--project-name NAME] <delete|deploy|show>"
-echo "   or $(basename "$0") [-T] <logs>"
-{V:HAVE_VOLUMES{
-echo "   or $(basename "$0") <provision> {O:VOLUME_NAME{[--mount-{{VOLUME_NAME/option}} DIRECTORY] }O}"
-}V}
-echo ""
-echo "Commands:"
-echo "   up         Start the application in the background"
-echo "   run        Run the application in the foreground (not available in swarm or kubernetes mode)"
-echo "   config     Display the configuration file that is sent to the underlying command"
-echo "   down       Stop the running application"
-echo "   logs       Display logs from all the containers (compose mode only)"
-{V:HAVE_VOLUMES{
-echo "   provision  Download thte external data into local directories so it can be mounted inside the running containers (docker compose only)"
-}V}
-echo ""
-echo "Options:"
-echo "    -p, --export-ports"
-echo "        Export the gRPC ports for all nodes"
-echo ""
-echo "    -r, --default-runtime"
-echo "        Ignore runtime settings and run with the default Docker runtime"
-echo ""
-echo "    -k, --kubernetes"
-echo "        Enable kubernetes mode"
-echo ""
-echo "    -S, --swarm"
-echo "        Enable swarm mode"
-echo ""
-echo "    -s, --skip-provision"
-echo "        Skip checking for new data files at startup"
-echo ""
-echo "    -T, --timestamps"
-echo "        Show timestamps in logs"
-echo ""
-echo "    --project-name NAME"
-echo "        Set the Docker Compose project name to NAME (default is $kd_PROJECT_NAME)"
-echo ""
-echo "    --grpc-port PORT  (or set {{NAME/id/upper}}_GRPC_PORT)"
-echo "        Override GRPC port (default is $grpc_PORT)"
-echo ""
-echo "    --rest-port PORT  (or set {{NAME/id/upper}}_REST_PORT)"
-echo "        Override REST API port (default is $rest_PORT)"
-echo ""
-echo "    --htdocs DIRECTORY  (or set {{NAME/id/upper}}_HTDOCS)"
-echo "        Directory with custom application files (default is \"${{NAME/id/upper}}_HTDOCS\")"
-echo ""
-{O:VOLUME_NAME{
-echo "    --mount-{{VOLUME_NAME/option-}} <DIRECTORY>  (or set {{VOLUME_NAME/id/upper}})"
-echo "        Override path to be mounted for {{VOLUME_NAME}} (Docker Compose only) (default is ${{VOLUME_NAME/id/upper}})"
-printf "        "{{VOLUME_COMMENT/sh}}"\n"
-echo ""
-echo "    --secret-{{VOLUME_NAME/option-}} <SECRET-NAME>  (or set {{VOLUME_NAME/id/upper}}_SECRET_NAME)"
-printf "        Secret name for COS access for {{VOLUME_NAME}}, default is \"${{VOLUME_NAME/id/upper}}_SECRET_NAME\"\n"
-echo ""
-echo "    --remote-{{VOLUME_NAME/option-}} <URL>  (or set {{VOLUME_NAME/id/upper}}_URL)"
-printf "        URL to the remote resource for {{VOLUME_NAME}}, default is \"${{VOLUME_NAME/id/upper}}_URL\"\n"
-echo ""
-}O}
-echo "    --htdocs <VOLUME-CLAIM-LABEL|CLOUD-OBJECT-STORE-URL>  (or set {{NAME/id/upper}}_HTDOCS)"
-echo "        Default is \"${{NAME/id/upper}}_HTDOCS\""
-echo "        Volume or COS URL for tarball with custom application files"
-echo ""
-echo "    --htdocs-secret-name <SECRET-NAME>  (or set {{NAME/id/upper}}_HTDOCS_SECRET_NAME)"
-printf "        Secret name for COS access for the custom application files, default is \"${{NAME/id/upper}}_HTDOCS_SECRET_NAME\"\n"
-echo ""
-echo "    --{{NAME}}-replicas <NUMBER>  (or set {{NAME/id/upper}}_REPLICAS)"
-echo "        Number of replicas for the main pod [{{MAIN_GROUP_NODES}}] default is $replicas_{{NAME/id/upper}}"
-echo ""
-{G:GROUP{
-echo "    --{{GROUP}}-replicas <NUMBER>  (or set {{NAME/id/upper}}_{{GROUP/id/upper}}_REPLICAS)"
-echo "        Number or replicas for pod \"{{GROUP}}\" [{{GROUP_NODES}}] default is $replicas_{{NAME/id/upper}}_{{GROUP/id/upper}}"
-echo ""
-}G}
-echo "Note: To access the Artifactory or Cloud Object Store $(basename $0) looks for the environment variable API_KEY."
-echo "If not found, it looks for a file named .api-key in the current directory and then in the home directory."
-echo ""
-exit 1
+    dd_display_help
+    exit 1
 fi
 provision() {
 {A:HAVE_COS{{{HAVE_COS}}
@@ -293,13 +303,17 @@ provision() {
     return 0
 }
 case "$1" in
+    help)
+        dd_display_help
+        exit 0
+        ;;
     provision)
         if [ -z "$use_COMPOSE" ]
         then
             provision
             exit $?
         else 
-            echo "provision is only available in docker compose  mode"
+            echo "provision is only available in Docker Compose mode"
             exit 1
         fi
         ;;
@@ -348,7 +362,7 @@ case "$1" in
             echo "$docker_COMPOSE_YAML" | envsubst | docker-compose -f - -p "$kd_PROJECT_NAME" logs -f $docker_compose_TIMESTAMPS
             exit $?
         else
-            echo "logs is only available in docker compose mode"
+            echo "logs are only available in Docker Compose mode"
             exit 1
         fi
         ;;
@@ -377,7 +391,7 @@ case "$1" in
             echo "$docker_COMPOSE_YAML" | envsubst | docker-compose -f - -p "$kd_PROJECT_NAME" up 
             rc=$?
         else
-            echo "run is only avaialble in docker compose mode"
+            echo "run is only avaialble in Docker Compose mode"
             exit 1
         fi
         ;;
