41e094eaad04b3db1cab738a05ad736789fe991f
Cezar Pendus <cpendus@us.ibm.com>
2021-07-04 13:37:23 -0400

    add special case for 2 files

--- flowc/vex.C
+++ flowc/vex.C
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include "vex.H"
+#include "stru1.H"
 
 struct envmap {
     static bool ends_with(std::string const &name, const std::string &suff) {
@@ -10,8 +11,6 @@ struct envmap {
         return name == "PATH" || ends_with(name, "_PATH");
     }
     std::pair<bool, std::string> getv(std::string const &name, int index=0) const {
-        std::cerr << " ENVIRON(" << name << ")["<< index << "]\n";
-
         char *envv = getenv(name.c_str());
         if(envv == nullptr)
             return std::make_pair(false, std::string());
@@ -61,28 +60,124 @@ static int usage(char const *bpath, int rc = 0) {
     return rc;
 }
 
+int read_json_file(std::map<std::string, std::vector<std::string>> &m, std::istream &is) {
+    int rc = 0;
+    auto jit = std::make_pair(std::istreambuf_iterator<char>(is), std::istreambuf_iterator<char>());
+    
+    bool accept = 0;
+    int state = 0;
+    std::string t, vn;
+    std::vector<std::string> vv;
+
+    while(!accept && rc == 0 && (t = stru1::scan_json(jit)) != "") {switch(state) {
+        case 0:
+            state = 1;
+            if(t != "{") ++rc;
+            break;
+        case 1:
+            vn = stru1::json_unescape(stru1::strip(t, "\""));
+            state = 2;
+            break;
+        case 2:
+            state = 3;
+            if(t != ":") ++rc;
+            break;
+        case 3:
+            state  = 4;
+            if(t != "[") ++rc;
+            vv.clear();
+            break;
+        case 4:
+            if(t != "]") {
+                state = 5;
+                vv.push_back(stru1::json_unescape(stru1::strip(t, "\"")));
+                break;
+            }
+        case 5:
+            if(t == ",") {
+                state = 4;
+            } else if(t == "]") {
+                state = 6;
+                m[vn] = vv;
+            } else {
+                ++rc;
+            }
+            break;
+        case 6:
+            if(t == ",") {
+                state = 1;
+            } else if(t == "}") {
+                state = 7;
+            } else {
+                ++rc;
+            }
+            break;
+        case 7:
+            accept = true;
+            break;
+
+    }
+    }
+    return rc;
+}
+
 int main(int argc, char *argv[]) {
     if(argc < 2) 
         return usage(argv[0], 0);
 
+    int stdin_used = 1;
     std::ifstream fin;
     std::istream *in = &std::cin;
     if(strcmp(argv[1], "-") != 0) {
+        stdin_used = 0;
         fin.open(argv[1]);
         if(!fin.is_open()) {
             std::cerr << argv[1] << ": error reading file\n";
             return 1;
         }
         in = &fin;
-    }
-    std::map<std::string, std::vector<std::string>> m;
-    
+    } 
     vex::lvmap maps;
-    auto jm = vex::make_vmap(vex::make_smap(m));
-    maps.push_front(&jm);
+    std::vector<std::map<std::string, std::vector<std::string>>> mv(argc-1);
+    auto jt = vex::make_vmap(vex::make_smap(mv[0]));
+    std::vector<decltype(jt)> mvs; 
 
-    envmap em;
+    for(int i = 2; i < argc; ++i) {
+        int rc = 0;
+        if(strcmp(argv[i], "-") == 0) {
+            if(stdin_used != 0) {
+                std::cerr << "stdin has already been used\n";
+                return 1;
+            }
+            stdin_used = i;
+            rc = read_json_file(mv[i-2], std::cin);
+            if(rc != 0) 
+                std::cerr << "<stdin>: error reading json variables\n";
+        } else {
+            std::ifstream fin;
+            fin.open(argv[i]);
+            if(!fin.is_open()) {
+                std::cerr << argv[i] << ": error reading file\n";
+                return 1;
+            }
+            rc = read_json_file(mv[i-2], fin);
+            if(rc != 0) 
+                std::cerr << argv[i] << ": error reading json variables\n";
+        }
+        if(rc != 0) {
+            return rc;
+        }
+        mvs.push_back(vex::make_vmap(vex::make_smap(mv[i-2])));
+    }
+    for(auto &m: mvs) 
+        maps.push_front(&m);
 
-    int rc = vex::expand(std::cout, *in, vex::make_cmap(jm, em));
+    int rc = 1;
+    if(mv.size() == 2) { 
+        rc = vex::expand(std::cout, *in, vex::make_cmap(vex::make_smap(mv.back()), vex::make_smap(mv[mv.size()-2])));
+    } else {
+        envmap em;
+        int rc = vex::expand(std::cout, *in, vex::make_cmap(maps, em));
+    }
     return rc;
 }
