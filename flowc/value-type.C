23f2536380a1f83e3c8247092c052ee8a28b6399
Cezar Pendus <cpendus@us.ibm.com>
2024-02-26 09:13:43 -0500

    Move the constructors out of the header

--- flowc/value-type.C
+++ flowc/value-type.C
@@ -4,6 +4,24 @@
 
 namespace fc {
 
+value_type::value_type(value_type const &a_val):
+    type(a_val.type), inf(a_val.inf), gname(a_val.gname), fname(a_val.fname) {
+}
+value_type::value_type(fvt_type t, std::string gn):type(t), gname(gn) {
+}
+value_type::value_type(fvt_type t, std::initializer_list<value_type> tl, std::string gn):
+    type(t), inf(tl), gname(gn) { 
+}
+value_type::value_type(int array_dim, value_type element_type) {
+    if(array_dim < 0) {
+        type = element_type.type;
+        inf = element_type.inf;
+        gname = element_type.gname;
+    } else {
+        type = fvt_array;
+        inf.push_back(value_type(array_dim-1, element_type));
+    }
+}
 
 bool value_type::operator ==(value_type const &other) const {
     switch(type) {
